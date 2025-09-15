ebaa63c32a213241fadb957782031575c0e9c6c7
aarsh487 <aanandpatel487@gmail.com>
2025-09-15 11:36:47 +0530

    post page hero section add gradient

--- src/components/Posts/CommunityHero.tsx
+++ src/components/Posts/CommunityHero.tsx
@@ -16,27 +16,28 @@ const CommunityHero: React.FC<CommunityHeroProps> = ({
   return (
     <section
       className={cn(
-        "flex flex-col sm:flex-row items-center sm:items-stretch sm:justify-between bg-medium text-white overflow-hidden relative",
+        "relative flex flex-col sm:flex-row items-center sm:items-stretch sm:justify-between overflow-hidden",
         className
       )}
     >
-      <div className="p-6 sm:p-12 max-w-lg flex-1 justify-end relative z-10">
-        <h2 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight uppercase">
+      {image && (
+        <Image
+          src={image}
+          fill
+          alt="Community Hero"
+          className="object-cover w-full h-full absolute inset-0"
+        />
+      )}
+
+      <div className="absolute inset-0 bg-gradient-to-r from-medium via-medium/60 to-transparent" />
+
+      <div className="relative z-10 p-6 sm:p-12 max-w-sm">
+        <h2 className="text-4xl sm:text-6xl lg:text-7xl font-extrabold leading-tight uppercase text-white">
           {text}
         </h2>
       </div>
 
-      {image && (
-        <div className="flex-1 w-full h-64 sm:h-auto relative">
-          <div className="absolute inset-0 bg-gradient-to-r from-medium via-medium/10 to-transparent pointer-events-none" />
-          <Image
-            src={image}
-            fill
-            alt="Community Hero"
-            className="object-cover w-full h-full"
-          />
-        </div>
-      )}
+      {/* <div className="flex-1" /> */}
     </section>
   );
 };
